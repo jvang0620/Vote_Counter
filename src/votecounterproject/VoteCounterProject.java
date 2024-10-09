60b92d9800579bbfef1ed89f460ce53ccecb39f3
Jai_Vang <jaivang0387@gmail.com>
2024-10-09 08:44:52 -0400

    first commit

---
+++ src/votecounterproject/VoteCounterProject.java
@@ -0,0 +1,10 @@
+package votecounterproject;
+
+public class VoteCounterProject {
+
+    public static void main(String[] args) {
+
+        ArrayListVoteCounter.runRandomElectionResults();
+    }
+
+}
